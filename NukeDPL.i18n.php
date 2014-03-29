07488ff1560c9919596841decaacb28c1fa6c83e
Raimond Spekking <raimond.spekking@gmail.com>
2014-03-29 13:46:32 +0100

    Some more consistency tweaks
    * use <code> instead of the depreceated <tt>
    * Sorint: 1. en, 2nd. qqq, 3. translations
    * Use {{int:...}} syntax for the button text
    * Add a FIXME for PLURAL usage
    
    Change-Id: Ic57dbf88d064b9bbf48f76d08e8040942c271ccb

--- NukeDPL.i18n.php
+++ NukeDPL.i18n.php
@@ -1,11 +1,14 @@
 <?php
+/**
+ * Internationalization file for the NukeDPL extension.
+ */
 
 $messages = array();
  
+/** English */
 $messages['en'] = array(
-	'right-nukedpl' => 'Nuke pages by Dynamic Page List queries',
 	'nukedpl' => 'Mass delete by DPL query',
-	'nukedpl-desc' => 'Adds a [[Special:NukeDPL|Special Page]] for mass deletion by DPL query',
+	'nukedpl-desc' => 'Adds a [[Special:NukeDPL|special page]] for mass deletion by DPL query',
 	'nukedpl-intro' => "This tool allows for mass deletions of pages selected by a DPL query.
 
 Enter a query below to generate a list of titles to delete.
@@ -14,10 +17,25 @@ Enter a query below to generate a list of titles to delete.
 * Queries shouldn't be surrounded by any DPL tags or braces.
 * For information about the parameter meanings, see the [http://semeb.com/dpldemo/index.php?title=DPL:Manual DPL Manual].",
 	'nukedpl-candidatelist' => 'View candidate list',
-	'nukedpl-nopages' => "No pages to delete using DPL-query: <tt>$1</tt>",
+	'nukedpl-nopages' => "No pages to delete using DPL-query: <code>$1</code>",
 	'nukedpl-nuke' => 'Nuke!',
-	'nukedpl-list' => "The following pages were selected by DPL-query: <tt>$1</tt> hit the button to delete them.",
+	'nukedpl-list' => 'The following pages were selected by DPL-query: <code>$1</code>
+	
+Hit "{{int:nukedpl-nuke}}" to delete them.', // FIXME: Add PLURAL for '...pages were...' 
 	'nukedpl-defaultreason' => "Mass removal of pages selected by DPL-query: ($1)",
+	'right-nukedpl' => 'Nuke pages by Dynamic Page List queries',
+);
+
+$messages['qqq'] = array(
+	'right-nukedpl' => '{{doc-right|nukedpl}}',
+	'nukedpl' => '{{doc-special|NukeDPL}}',
+	'nukedpl-desc' => '{{desc|name=NukeDPL|url=https://www.mediawiki.org/wiki/Extension:NukeDPL}}',
+	'nukedpl-intro' => 'Text explaining how to use the special page',
+	'nukedpl-candidatelist' => 'Text in the button that generates the list of pages to be deleted',
+	'nukedpl-nopages' => 'Message when the query returns no pages to be deleted',
+	'nukedpl-nuke' => 'Text in the button to delete the selected pages',
+	'nukedpl-list' => 'Text preceding the list of pages to be deleted',
+	'nukedpl-defaultreason' => 'Default reason associated with the deleted pages',
 );
 
 $messages['de'] = array(
@@ -35,16 +53,4 @@ Geben Sie unten eine DPL-Query ein, um eine Liste mit den zu löschenden Seiten 
 	'nukedpl-nuke' => 'Löschen!',
 	'nukedpl-list' => "Die folgenden Seiten wurden gefunen. DPL-Query: <tt>$1</tt><br />Button drücken, um diese zu löschen.",
 	'nukedpl-defaultreason' => "Massenlöschung von Seiten mittels DPL-Query: ($1)",
-);
-
-$messages['qqq'] = array(
-	'right-nukedpl' => '{{doc-right|nukedpl}}',
-	'nukedpl' => '{{doc-special|NukeDPL}}',
-	'nukedpl-desc' => '{{desc|name=NukeDPL|url=https://www.mediawiki.org/wiki/Extension:NukeDPL}}',
-	'nukedpl-intro' => 'Text explaining how to use the special page',
-	'nukedpl-candidatelist' => 'Text in the button that generates the list of pages to be deleted',
-	'nukedpl-nopages' => 'Message when the query returns no pages to be deleted',
-	'nukedpl-nuke' => 'Text in the button to delete the selected pages',
-	'nukedpl-list' => 'Text preceding the list of pages to be deleted',
-	'nukedpl-defaultreason' => 'Default reason associated with the deleted pages',
 );
